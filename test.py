d2b9889b60f49882fab528b08e7f61504f9bb50d
JoshDevx <josmdev@gmail.com>
2026-03-26 21:28:38 -0500

    fix: remover carpeta venv del rastreo de git

---
+++ test.py
@@ -0,0 +1,5 @@
+import sys
+
+print("¡Hola, Data Engineer! El entorno está funcionando.")
+print("-------------------------------------------------")
+print(f"Ruta exacta de Python que estamos usando:\n{sys.executable}")
