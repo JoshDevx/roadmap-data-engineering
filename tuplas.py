3996ee47bf6740680be98316260db02b9cd047cd
JoshDevx <josmdev@gmail.com>
2026-03-28 18:46:38 -0500

    Python: Tuplas

---
+++ tuplas.py
@@ -0,0 +1,10 @@
+# son inmutables
+tupla = (100,200,300,400, 400,400, [1,2,3])
+
+print(tupla) 
+
+# index para buscar un elemento
+print(tupla.index(100)) # Nos muestra el indice donde se encuentra el valor, no esta nos dara error
+
+# Contar el numero de elementos
+print(tupla.count(400)) 
